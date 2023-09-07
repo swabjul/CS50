50d56db792ff92aad1c3461c44f840e109f0cdc6
Swab Jul <zj.bulawan@gmail.com>
2023-09-07 09:19:19 +0800

    Uploading Python folder

---
+++ python/dictionaries.py
@@ -0,0 +1,9 @@
+houses = {"Harry": "Gryffindor",
+          "Draco": "Slytherin"}
+print(houses)
+
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Hermione"])
+
+
---
+++ python/sequences.py
@@ -0,0 +1,14 @@
+
+# List sequence of mutable values / same as array
+names = ["Harry", "Ron", "Hermione"]
+
+print(names[1])
+
+# Tuple sequence of immutable values / fixed values
+coordinate = (10.0, 11.0)
+print(coordinate)
+
+# Set collection of unique values /  
+
+
+# dict collection of key-value pairs
